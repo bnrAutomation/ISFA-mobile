d2ebbfc42c39315df6c6981dc31b9c1dbc4c33c7
Gopal krishan <gplkrishan727@gmail.com>
2023-04-12 20:14:07 +0530

    merge changes

--- lib/module/tabber_module/tabber_view.dart
+++ lib/module/tabber_module/tabber_view.dart
@@ -192,41 +192,10 @@ class AppSideMenu extends StatelessWidget {
           UserAccountsDrawerHeader(
               decoration: BoxDecoration(color: Theme.of(context).primaryColor),
               currentAccountPicture: const CircleAvatar(),
-              otherAccountsPictures: [
-                BlocBuilder<TabberBloc, TabberState>(
-                  buildWhen: (previous, current) =>
-                      (current is OnlineStatusUpdateState),
-                  builder: (context, state) {
-                    final bloc = context.read<TabberBloc>();
-                    return FittedBox(
-                      child: AppStorage().userDetail?.designation == "fwp"
-                          ? Column(
-                              children: [
-                                CupertinoSwitch(
-                                    value: bloc.isOnline,
-                                    onChanged: (newVal) async {
-                                      Scaffold.of(context).closeDrawer();
-                                      final XFile? image = await context
-                                          .pushNamed(AppPaths.checkin);
-                                      if (newVal) {
-                                        bloc.add(
-                                            StartDutyStatusTabberEvent(image));
-                                      } else {
-                                        bloc.add(
-                                            EndDutyStatusTabberEvent(image));
-                                      }
-                                    }),
-                                Text(
-                                  bloc.isOnline ? "On-Duty" : "Off-Duty",
-                                  style: const TextStyle(color: Colors.white),
-                                )
-                              ],
-                            )
-                          : const SizedBox(),
-                    );
-                  },
-                )
-              ],
+              otherAccountsPictures:
+                  AppStorage().userDetail?.designation != "fwp"
+                      ? null
+                      : [dutyStatus()],
               accountName: Text(data.userInfo.companyName),
               accountEmail: Text(data.userInfo.userName)),
           ...data.menu.where((element) => element.isActive).map(
@@ -317,6 +286,36 @@ class AppSideMenu extends StatelessWidget {
           ),
         ],
       ),
+    );
+  }
+
+  Widget dutyStatus() {
+    return BlocBuilder<TabberBloc, TabberState>(
+      buildWhen: (previous, current) => (current is OnlineStatusUpdateState),
+      builder: (context, state) {
+        final bloc = context.read<TabberBloc>();
+        return FittedBox(
+            child: Column(
+          children: [
+            CupertinoSwitch(
+                value: bloc.isOnline,
+                onChanged: (newVal) async {
+                  Scaffold.of(context).closeDrawer();
+                  final XFile? image =
+                      await context.pushNamed(AppPaths.checkin);
+                  if (newVal) {
+                    bloc.add(StartDutyStatusTabberEvent(image));
+                  } else {
+                    bloc.add(EndDutyStatusTabberEvent(image));
+                  }
+                }),
+            Text(
+              bloc.isOnline ? "On-Duty" : "Off-Duty",
+              style: const TextStyle(color: Colors.white),
+            )
+          ],
+        ));
+      },
     );
   }
 
