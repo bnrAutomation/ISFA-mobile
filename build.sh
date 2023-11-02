98804079c360c789cfa3fc7d16be9ff89f2d6297
Gopal krishan <gplkrishan727@gmail.com>
2023-11-02 21:21:56 +0530

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,3 @@
+flutter build ios --release --dart-define BASE_URL=https://isfaqa.denave.com:8445
+flutter build ipa --release --dart-define BASE_URL=https://isfaqa.denave.com:8445
+open build/ios/ipa/
