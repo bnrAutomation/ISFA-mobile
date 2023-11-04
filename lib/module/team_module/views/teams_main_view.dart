f6fe0faa710b5d51b5959225838453b4bf3553e5
vishaldenave <vishaldenave@gmail.com>
2023-11-04 13:36:17 +0530

    update team view

--- lib/module/team_module/views/teams_main_view.dart
+++ lib/module/team_module/views/teams_main_view.dart
@@ -3,7 +3,6 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:i_densfa/module/team_module/bloc/team_bloc.dart';
 import 'package:i_densfa/module/team_module/views/team_hierarchy_view.dart';
 import 'package:i_densfa/module/team_module/views/team_list_view.dart';
-import 'package:i_densfa/utility/app_constants.dart';
 import 'package:i_densfa/utility/extensions.dart';
 
 class TeamMainView extends StatelessWidget {
@@ -31,24 +30,28 @@ class TeamMainView extends StatelessWidget {
               final TeamBloc teamBloc = context.read();
               return Column(
                 children: [
-                  Row(
-                    children: [
-                      TeamTabButton(
-                        isSelected: teamBloc.isHierarchyView,
-                        title: 'Hierarchy View',
-                        onPressed: () =>
-                            teamBloc.add(ChangeViewStyleTeamEvent(true)),
-                      ),
-                      const SizedBox(width: 2),
-                      TeamTabButton(
-                        isSelected: !teamBloc.isHierarchyView,
-                        title: 'List View',
-                        onPressed: () {
-                          teamBloc.add(GetTeamDataEvent());
-                          teamBloc.add(ChangeViewStyleTeamEvent(false));
-                        },
-                      ),
-                    ],
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 5.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        TeamTabButton(
+                          isSelected: teamBloc.isHierarchyView,
+                          title: 'Hierarchy View',
+                          onPressed: () =>
+                              teamBloc.add(ChangeViewStyleTeamEvent(true)),
+                        ),
+                        const SizedBox(width: 2),
+                        TeamTabButton(
+                          isSelected: !teamBloc.isHierarchyView,
+                          title: 'List View',
+                          onPressed: () {
+                            teamBloc.add(GetTeamDataEvent());
+                            teamBloc.add(ChangeViewStyleTeamEvent(false));
+                          },
+                        ),
+                      ],
+                    ),
                   ),
                   Expanded(
                     child: teamBloc.isHierarchyView
@@ -79,13 +82,17 @@ class TeamTabButton extends StatelessWidget {
     return Expanded(
       child: ElevatedButton(
         style: ElevatedButton.styleFrom(
-            backgroundColor:
-                isSelected ? ColorConstants.amber : ColorConstants.amberFade,
-            elevation: 4,
+            backgroundColor: isSelected
+                ? Theme.of(context).primaryColor
+                : Colors.transparent,
+            elevation: isSelected ? 4 : 0,
             shape:
                 RoundedRectangleBorder(borderRadius: BorderRadius.circular(8))),
         onPressed: onPressed,
-        child: Text(title),
+        child: Text(
+          title,
+          style: const TextStyle(color: Colors.black),
+        ),
       ),
     );
   }
